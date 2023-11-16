a60af1a6df8907264b3a5d9f842e1b23bb7412a4
TCTrung <trungtrinh1325@gmail.com>
2023-11-16 14:53:14 +0700

    hi

---
+++ demo1/demo1.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE `demo2`;
+USE `demo2`;
+CREATE TABLE `class`(
+id int,
+name varchar(200)
+);
+CREATE TABLE `teacher`(
+id int,
+name varchar(200),
+age int,
+country varchar(50)
+);
